746ddbd9a0eda2403083c16daaf993e5adc4d617
Thomas Le Veo <thomasleveo@gmail.com>
2023-11-11 12:32:06 +0100

    PropTypes

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,35 @@
+import DefaultPicture from '../../assets/profile.png'
+import Card from '../../components/Card'
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Developpeur frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jeanne Biche',
+    jobTitle: 'Développeuse Fullstack',
+    picture: DefaultPicture,
+  },
+]
+
 function Freelance() {
   return (
     <div>
-      <h1>Page de Freelances</h1>
+      <h1>Freelances</h1>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          title={profile.name}
+        />
+      ))}
     </div>
   )
 }
